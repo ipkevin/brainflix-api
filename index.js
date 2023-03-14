93e109710b6ce96a78f39cafe863fd21b59c01f9
Kevin Ip <ipkevin@gmail.com>
2023-03-14 16:00:09 -0700

    add initial routes for getting videos and specific video

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require("express");
 const app = express();
 const port = 8080;
 
+const videoData = require('./data/video-details.json');
+
+app.use(express.static('public'));
+app.use(express.json());
+
 app.listen(port, () => {
     console.log("Server is now listening on port 8080");
 })
@@ -12,4 +17,27 @@ app.route("/")
 })
 .post((req, res) => {
     res.send("POST request received!");
+})
+
+app.route("/videos")
+.get((req,res) => {
+    let filteredVideos = videoData.map((item) => {
+        return (
+            {
+                id: item.id,
+                title: item.title,
+                channel: item.channel,
+                image: item.image
+            }
+        )})
+    res.json(filteredVideos);
+})
+
+app.route("/videos/:id")
+.get((req, res) => {
+    res.json(videoData.filter((item) => {
+        return (
+            item.id === req.params.id
+        )
+    }))
 })
