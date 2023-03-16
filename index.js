b67f2be4ece9927d8f4a5ffd9922e943118f446e
Kevin Ip <ipkevin@gmail.com>
2023-03-15 21:47:17 -0700

    clean up unused routes/verbs in index.js. Remove commented out code.

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const port = 8080;
 const cors = require("cors");
 const videosRoute = require('./routes/videos');
 
-// const videoData = require('./data/video-details.json');
-
 app.use(express.static('public'));
 app.use(express.json());
 app.use(cors());
@@ -15,34 +13,8 @@ app.use("/videos", videosRoute);
 
 app.route("/")
 .get((req,res) => {
-    res.send("We got your request. Sit tight and we'll be with you shortly.");
+    res.status(204).send("We got your request but we're not going to do anything about it. Any plans for the summer? Mooo.");
 })
-.post((req, res) => {
-    res.send("POST request received!");
-})
-
-// app.route("/videos")
-// .get((req,res) => {
-//     let filteredVideos = videoData.map((item) => {
-//         return (
-//             {
-//                 id: item.id,
-//                 title: item.title,
-//                 channel: item.channel,
-//                 image: item.image
-//             }
-//         )})
-//     res.json(filteredVideos);
-// })
-
-// app.route("/videos/:id")
-// .get((req, res) => {
-//     let videoSelected = videoData.filter(item => {
-//         return (item.id === req.params.id)
-//     })
-//     res.json(videoSelected[0]);
-// })
-
 
 app.listen(port, () => {
     console.log("Server is now listening on port 8080");
